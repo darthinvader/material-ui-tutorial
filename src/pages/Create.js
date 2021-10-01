0afcc67c8df4e939bdb6f429ee43da541072524a
darthinvader <psaradakisj@gmail.com>
2021-10-01 13:35:55 +0300

    [feat] typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import { Typography } from "@material-ui/core";
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography variant="h1" color="primary" align="center">
+        Create A New Note
+      </Typography>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+        align="center"
+        noWrap
+      >
+        ajsd ajshd ashd lkasdlas hdklash dasgdlagsdljgasljdajsd ajshd ashd
+        lkasdlas hdklash dasgdlagsdljgasljdajsd ajshd ashd lkasdlas hdklash
+        dasgdlagsdljgasljdajsd ajshd ashd lkasdlas hdklash
+        dasgdlagsdljgasljdajsd ajshd ashd lkasdlas hdklash
+        dasgdlagsdljgasljdajsd ajshd ashd lkasdlas hdklash
+        dasgdlagsdljgasljdajsd ajshd ashd lkasdlas hdklash dasgdlagsdljgasljd
+      </Typography>
     </div>
-  )
+  );
 }
